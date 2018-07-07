473d180d63fbab1961f53b155ba354e52daf2e40
brinkr <memoever@msn.com>
2018-07-07 14:23:14 +0800

    push

---
+++ mine-admin-v1/assets/json/console/.svn/text-base/top-search.js.svn-base
@@ -0,0 +1,46 @@
+{
+  "code": 0
+  ,"msg": ""
+  ,"count": "100"
+  ,"data": [{
+    "keywords": "李玮锋是男是女"
+    ,"frequency": 8520
+    ,"userNums": 2216
+  },{
+    "keywords": "李玮锋是男是女"
+    ,"frequency": 666
+    ,"userNums": 333
+  },{
+    "keywords": "李玮锋是男是女"
+    ,"frequency": 666
+    ,"userNums": 333
+  },{
+    "keywords": "李玮锋是男是女"
+    ,"frequency": 666
+    ,"userNums": 333
+  },{
+    "keywords": "李玮锋是男是女"
+    ,"frequency": 666
+    ,"userNums": 333
+  },{
+    "keywords": "李玮锋是男是女"
+    ,"frequency": 666
+    ,"userNums": 333
+  },{
+    "keywords": "李玮锋是男是女"
+    ,"frequency": 666
+    ,"userNums": 333
+  },{
+    "keywords": "李玮锋是男是女"
+    ,"frequency": 666
+    ,"userNums": 333
+  },{
+    "keywords": "李玮锋是男是女"
+    ,"frequency": 666
+    ,"userNums": 333
+  },{
+    "keywords": "李玮锋是男是女"
+    ,"frequency": 666
+    ,"userNums": 333
+  }]
+}
---
+++ mine-admin-v1/assets/json/console/top-card.js
@@ -0,0 +1,76 @@
+{
+  "code": 0
+  ,"msg": ""
+  ,"count": "100"
+  ,"data": [{
+    "id": 111
+    ,"title": "深度解读李玮锋女装之谜"
+    ,"username": "萎疯"
+    ,"channel": "公告"
+    ,"href": "#"
+    ,"crt": 61632
+  },{
+    "id": 222
+    ,"title": "深度解读李玮锋女装之谜"
+    ,"username": "猫吃"
+    ,"channel": "讨论"
+    ,"href": "#"
+    ,"crt": 61632
+  },{
+    "id": 333
+    ,"title": "深度解读李玮锋女装之谜"
+    ,"username": "fd"
+    ,"channel": "分享"
+    ,"href": "#"
+    ,"crt": 61632
+  },{
+    "id": 333
+    ,"title": "深度解读李玮锋女装之谜"
+    ,"username": "纸飞机"
+    ,"channel": "提问"
+    ,"href": "#"
+    ,"crt": 61632
+  },{
+    "id": 333
+    ,"title": "如深度解读李玮锋女装之谜"
+    ,"username": "纸飞机"
+    ,"channel": "提问"
+    ,"href": "#"
+    ,"crt": 61632
+  },{
+    "id": 333
+    ,"title": "深度解读李玮锋女装之谜"
+    ,"username": "纸飞机"
+    ,"channel": "提问"
+    ,"href": "#"
+    ,"crt": 61632
+  },{
+    "id": 333
+    ,"title": "深度解读李玮锋女装之谜"
+    ,"username": "纸飞机"
+    ,"channel": "提问"
+    ,"href": "#"
+    ,"crt": 61632
+  },{
+    "id": 333
+    ,"title": "深度解读李玮锋女装之谜"
+    ,"username": "纸飞机"
+    ,"channel": "提问"
+    ,"href": "#"
+    ,"crt": 61632
+  },{
+    "id": 333
+    ,"title": "深度解读李玮锋女装之谜"
+    ,"username": "纸飞机"
+    ,"channel": "提问"
+    ,"href": "#"
+    ,"crt": 61632
+  },{
+    "id": 333
+    ,"title": "深度解读李玮锋女装之谜"
+    ,"username": "纸飞机"
+    ,"channel": "提问"
+    ,"href": "#"
+    ,"crt": 61632
+  }]
+}
